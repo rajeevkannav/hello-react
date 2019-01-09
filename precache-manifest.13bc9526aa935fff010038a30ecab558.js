fbba890d8fe88e4d2ef2a58268e5aa79749519af
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-01-09 13:49:51 +0530

    Updates

---
+++ precache-manifest.13bc9526aa935fff010038a30ecab558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "515087f1a8298fcd26da",
+    "url": "/hello-react/static/css/main.5ad3b2ca.chunk.css"
+  },
+  {
+    "revision": "515087f1a8298fcd26da",
+    "url": "/hello-react/static/js/main.515087f1.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/hello-react/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "2ac5259eac6c393b99aa",
+    "url": "/hello-react/static/js/runtime~main.2ac5259e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "62aaa938db3c5ec59a00df990776c37f",
+    "url": "/hello-react/index.html"
+  }
+];
